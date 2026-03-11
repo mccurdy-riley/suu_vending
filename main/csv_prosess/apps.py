2490279c3b90b8b2dc18717eaa77c71aa5ff8121
Riley McCurdy <mccurdyriley@protonmail.com>
2026-03-11 11:13:24 -0600

    Updated format

--- main/csv_prosess/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CsvProsessConfig(AppConfig):
-    name = 'csv_prosess'
--- main/csv_prosess/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class UploadFileForm(forms.Form):
-    csv_file = forms.FileField(
-        label="Select Cashless Transactions CSV",
-        help_text="Upload the raw export from your vending system.",
-    )

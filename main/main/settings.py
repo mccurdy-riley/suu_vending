0779235f50f6fca0be0a1fc9620e946406214a47
Riley McCurdy <mccurdyriley@protonmail.com>
2026-03-11 11:00:49 -0600

    Updated for Deployment

--- main/main/settings.py
+++ main/main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-4g$$t@yl434-9j@&abw(9lgzq72u!5coo5y&2z+)!w*z$p!)%%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # Correct position
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -117,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# This ensures Django doesn't break if you don't have a 'static' folder created yet
+STATICFILES_DIRS = [
+    # BASE_DIR / "static", # Uncomment this if you created a 'static' folder in your project
+]
